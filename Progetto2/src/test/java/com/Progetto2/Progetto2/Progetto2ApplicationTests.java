c7e5775b41d047f6a539cac40d04e9f4a678ebf7
unknown <nicolas.demicco@gmail.com>
2021-09-02 14:54:54 +0200

    inizializzzazione progetto 2

---
+++ Progetto2/src/test/java/com/Progetto2/Progetto2/Progetto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Progetto2.Progetto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progetto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
